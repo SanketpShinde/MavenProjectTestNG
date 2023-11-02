a2202dba5a96fa995d56dca4d82e823d380e388c
Mahesh0421 <maheshjagdale211199@gmail.com>
2023-11-02 14:34:28 +0530

    [Mahesh]Pull Request All Steps are add in Src/main/java ->App.java class

--- src/main/java/MevanProjectPractice/MavenProjectTestNG/App.java
+++ src/main/java/MevanProjectPractice/MavenProjectTestNG/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "Hello Pull Request Is done "
+        		+ "Step 1: Search Git Repo"
+        		+ "Step 2: Click on Clone a git Repo icon(Yellow Icon)"
+        		+ "Step 3: Source git Repo New Windo Open"
+        		+ "Step 4: Enter Repo URI (Which we Need to pull repo ) "
+        		+ "Step 5: Enter User ID and Password(token)"
+        		+ "Step 6: After Entering user ID And Pass Next page will appear hear we just Clcik on Next button than click on finish Button"
+        		+ "Step 7: Clone Repo get Created in our Local Git Repositories NOW We need to import this code in our Package Explorer"
+        		+ "Clcik on FILE > IMPORT > Click GIT folder(Dropdown) >Click Project from git option > Clcik on Existing local repo > Clcik next Button >Slect our project which we need to import from local git repo> Clcik on next button> Clcik on Finish Button"
+        		+ "Our Project is added in Package Explorer" );
     }
 }
